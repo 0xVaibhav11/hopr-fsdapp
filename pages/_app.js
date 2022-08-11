95fcb0edc1c6257a1455beb28de0183167aa4fc3
Vaibhav Anand <vanoobcoder@gmail.com>
2022-08-12 03:56:53 +0530

    UI Change

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,7 @@
 import "../styles/globals.scss";
-import { motion, AnimatePresence } from "framer-motion";
 
-function MyApp({ Component, pageProps, router }) {
-  return (
-    <AnimatePresence>
-      <motion.div
-        key={router.route}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        // varients={{
-        //   pageInitial: {
-        //     width: 0,
-        //     opacity: 1,
-        //   },
-        //   pageAnimate: {
-        //     width: "100%",
-        //     opacity: 0,
-        //   },
-        //   pageExit: {
-        //     x: window.innerWidth,
-        //     opacity: 0,
-        //   },
-        // }}
-      >
-        <Component {...pageProps} />
-      </motion.div>
-    </AnimatePresence>
-  );
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
